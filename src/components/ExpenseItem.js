ea815781c8e46e01c51803a88336d559dc4d2f13
suraj <surajsah1215@gmail.com>
2023-10-17 16:06:48 +0530

    passing data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,20 @@
+import './ExpenseItem.css'
 function ExpenseItem(){
-    return <h2>Expnse Item!</h2>
+    const locationOfexpenditure = "Delhi";
+
+    return (
+        
+        <div className="expense-item"> 
+            <h3 className="expense-item">Expense Item!</h3>
+            <h3>Food Rs 10</h3>
+            <h3>Petro Rs 100</h3>
+            <h3>Movies Rs 200</h3>
+            <h3>{locationOfexpenditure}</h3>
+
+        </div>
+    
+
+    )
 }
 
 export default ExpenseItem;
