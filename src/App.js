312fc8ea5a8b1398760c2ee8e130939615131919
suraj <surajsah1215@gmail.com>
2023-10-25 14:01:44 +0530

    making components smaller and redable

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       
           <ExpenseItem></ExpenseItem>
+          <ExpenseItem></ExpenseItem>
        
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,18 @@
 import './ExpenseItem.css'
-function ExpenseItem(){
+function ExpenseItem(props){
     const locationOfexpenditure = "Delhi";
+    const month = props.date.toLocalString('en-US',{month:'long'});
+    const year = props.date.toFullYear();
+    const day = props.date.toLocalString('en-US',{day:'2-digit'});
 
     return (
         
         <div className="expense-item"> 
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <h3 className="expense-item">Expense Item!</h3>
             <h3>Food Rs 10</h3>
             <h3>Petro Rs 100</h3>
